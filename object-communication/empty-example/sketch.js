ccc428d8516f713f2e949dbff1a3854823fc46b1
dithermedaddy <moi@faanross.com>
2022-12-31 09:38:51 -0500

    message

---
+++ object-communication/empty-example/sketch.js
@@ -0,0 +1,58 @@
+let bubble1;
+let bubble2;
+
+function setup(){
+  createCanvas(600, 600);
+  bubble1 = new Bubble(200, 200, 50);
+  bubble2 = new Bubble(300, 300, 50);
+}
+
+function draw(){
+  background(0);
+  let d = dist(bubble1.x, bubble1.y, bubble2.x, bubble2.y);
+  if (d < bubble1.r + bubble2.r){
+    background(200, 0, 100);
+  }
+  else {
+  bubble1.show();
+  bubble1.move();
+  bubble2.show();
+  bubble2.move();
+ }
+}
+
+class Bubble{
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.brightness = 0;
+  }
+
+  changeColor(bright) {
+    this.brightness = bright;
+  }
+
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    if (d < this.r) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    fill(this.brightness, 125);
+    ellipse(this.x, this.y, this.r*2);
+  }
+
+}
