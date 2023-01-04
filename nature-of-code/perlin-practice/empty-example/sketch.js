a5b98c227333797e967debcb2260579a6fbd809f
dithermedaddy <moi@faanross.com>
2023-01-03 21:02:40 -0500

    perlin function nature of code basics

--- nature-of-code/perlin-practice/empty-example/sketch.js
+++ nature-of-code/perlin-practice/empty-example/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  // put setup code here
+  createCanvas(600, 600);
+  frameRate(20);
 }
 
 function draw() {
-  // put drawing code here
+  background(69);
+  // let x = random(600);
+  // let x = 600*noise(100);
+
+  let x = map(noise(100), 0, 1, 0, width);
+
+  ellipse(x, 300, 24, 24);
 }
