4c7fb3907d6bcc0c1006983064fe00529bc6a9cc
dithermedaddy <moi@faanross.com>
2023-01-04 20:17:18 -0500

    updates to beavis walking animation

--- coding-challenge/111_animated_sprite/beavis/sketch.js
+++ coding-challenge/111_animated_sprite/beavis/sketch.js
@@ -1,7 +1,55 @@
+let beavis = [];
+let counter = 0; // counter variable to keep track of the current index
+let timer = 0; // timer variable to control the amount of time each image is displayed
+let mover = true; 
+let x = 51;
+
+function preload() {
+  for (let i = 0; i < 9; i++) {
+    beavis[i] = loadImage(i + ".png");
+  }
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(600, 600);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  let img = beavis[counter];
+  timer++; // increment the timer
+  if (timer > 5) { // if the timer has reached 30 (30 * 1/frameRate() = 0.3 seconds)
+    timer = 0; // reset the timer
+    counter++; // increment the counter
+    if (counter > 8) { // if the counter has reached 8 (the last index in the array)
+      counter = 0; // reset the counter
+    }
+  }
+
+  if (mover == true)
+  {
+    x = x + 2;
+  }
+
+  if (mover == false)
+  {
+    x = x - 2; 
+  }
+
+  if (x > width-50)
+  {
+    mover = false;
+  }
+
+  if (x < 50)
+  {
+    mover = true;
+  }
+  
+  // img.mirror(true, false);
+
+  image(img, x, 50); // display the image at the current index
+
+
 }
+
