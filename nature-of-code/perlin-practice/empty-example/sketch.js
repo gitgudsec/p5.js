98a8f7c6618830e2745fea0cdd728274813e4058
dithermedaddy <moi@faanross.com>
2023-01-05 20:30:55 -0500

    perlin continue

--- nature-of-code/perlin-practice/empty-example/sketch.js
+++ nature-of-code/perlin-practice/empty-example/sketch.js
@@ -1,14 +1,11 @@
-let xoff = 0;
-let yoff = 0;
-let cage = []; 
 
-function preload(){
-  for (let k = 0; k < 1000; k++)
-  {
-    cage[k] = loadImage("cage2.png");
-  }
+// function preload(){
+//   for (let k = 0; k < 1000; k++)
+//   {
+//     cage[k] = loadImage("cage2.png");
+//   }
   
-}
+
 
 function setup() {
   createCanvas(600, 600);
@@ -17,25 +14,9 @@ function setup() {
 
 function draw() {
   background(69);
+  ellipse(300, 300, 40, 40)
 
-  a = random(2000);
-  b = random(2000);
-  cager(a, b);
-  a = random(600);
-  b = random(600);
-  cager(a, b);
 
-  xoff -= 0.552; 
-  yoff -= 0.159;
 
 }
 
-function cager(a, b){
-  let x = map(noise(xoff), 0, 1, 0, width);
-  let y = map(noise(yoff), 0, 1, 0, height);
-for (let k = 0; k < 1000; k++)
-{
-  
-  image(cage[k], x+a, y+b, 200, 200);
-}
-}
