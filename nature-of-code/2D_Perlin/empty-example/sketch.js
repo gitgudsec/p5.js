96d2e440edfadc6667d9ea8d4cca42c95c5c3e25
dithermedaddy <moi@faanross.com>
2023-01-08 16:14:10 -0500

    pixel array

--- nature-of-code/2D_Perlin/empty-example/sketch.js
+++ nature-of-code/2D_Perlin/empty-example/sketch.js
@@ -1,20 +1,19 @@
 var inc = 0.01;
 
-
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
-  loadPixels; 
+  loadPixels(); 
   background(51);
   stroke(255);
   noFill();
   beginShape();
   for (var x = 0; x < width; x++)
   {
-    
-  }
 
+  }
+  endShape();
 
 }
