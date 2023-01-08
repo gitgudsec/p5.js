2591992c6bea9c9285ab276e43197dd05a7ac2f5
dithermedaddy <moi@faanross.com>
2023-01-08 16:51:03 -0500

    pixel array

--- nature-of-code/pixel_array/empty-example/sketch.js
+++ nature-of-code/pixel_array/empty-example/sketch.js
@@ -1,7 +1,19 @@
+
 function setup() {
-  // put setup code here
+  createCanvas(600, 600);
 }
 
 function draw() {
-  // put drawing code here
+// we have to inform p5.js we want to work with "the pixel"
+background(109, 231, 100);
+loadPixels();
+
+for (let i = 1000000; i < 10000000; i++)
+{
+  pixels[i] = 255;
+}
+
+
+updatePixels();
+
 }
