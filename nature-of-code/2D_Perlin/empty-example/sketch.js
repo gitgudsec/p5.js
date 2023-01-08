8c2a3181d1ab673b28cedca1ecb324d951b69626
dithermedaddy <moi@faanross.com>
2023-01-08 16:09:49 -0500

    2D Perlin

---
+++ nature-of-code/2D_Perlin/empty-example/sketch.js
@@ -0,0 +1,20 @@
+var inc = 0.01;
+
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  loadPixels; 
+  background(51);
+  stroke(255);
+  noFill();
+  beginShape();
+  for (var x = 0; x < width; x++)
+  {
+    
+  }
+
+
+}
