5b4326feb7ad9c006ab9392c2d58245d8a727a57
dithermedaddy <moi@faanross.com>
2023-01-08 15:48:09 -0500

    perlin continue

--- website_tests/001_add pterodactyl/ptero/sketch.js
+++ website_tests/001_add pterodactyl/ptero/sketch.js
@@ -1,14 +1,13 @@
-let beavis = [];
+let ptero = [];
 let counter = 0; // counter variable to keep track of the current index
 let timer = 0; // timer variable to control the amount of time each image is displayed
-let mover = true; 
 let xoff = 0;
 let yoff = 0;
 
 
 function preload() {
-  for (let i = 0; i < 9; i++) {
-    beavis[i] = loadImage(i + ".png");
+  for (let i = 3; i >= 0; i--) {
+    ptero[i] = loadImage(i + ".png");
   }
 }
 
@@ -18,12 +17,12 @@ function setup() {
 
 function draw() {
   background(0);
-  let img = beavis[counter];
+  let img = ptero[counter];
   timer++; // increment the timer
-  if (timer > 5) { //ontrols walking speed
+  if (timer > 5) { // if the timer has reached 30 (30 * 1/frameRate() = 0.3 seconds)
     timer = 0; // reset the timer
     counter++; // increment the counter
-    if (counter > 8) { // if the counter has reached 8 (the last index in the array)
+    if (counter > 3) { // if the counter has reached 8 (the last index in the array)
       counter = 0; // reset the counter
     }
   }
@@ -38,4 +37,3 @@ function draw() {
 
 
 }
-
