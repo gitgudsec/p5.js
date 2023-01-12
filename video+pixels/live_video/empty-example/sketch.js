cf8374b961084c80226917ce860c087a0dabaddc
dithermedaddy <moi@faanross.com>
2023-01-11 19:45:04 -0500

    video and pixels course continue

--- video+pixels/live_video/empty-example/sketch.js
+++ video+pixels/live_video/empty-example/sketch.js
@@ -1,14 +1,17 @@
 var video;
+var button; 
 
 function setup() {
-  createCanvas(320, 240);
-  background(69)
+  // createCanvas(320, 240);
+  // background(69)
   video = createCapture(VIDEO);
   video.size(320, 240)
+  button = createButton('snap')
+  // video.hide()
 }
 
 function draw() {
-  video.loadPixels();
-  tint(255, 0, 150);
- image(video, 0, 0);
+//   video.loadPixels();
+//   tint(255, 0, 150);
+//  image(video, 0, 0, mouseX, 240);
 }
